5a5714a4aee401986dbacf00279f018e344ac4ff
Atlant5000 <maximmmd13@gmail.com>
2024-11-25 17:49:56 +0200

    Task 3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Value();
+        }
+        public static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        public static double Func(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+        static void Value()
+        {
+            int number1 = Input("a=");
+            int number2 = Input("b=");
+            Console.WriteLine($"Наибольшее число = {Func(number1, number2)}");
+            Console.ReadLine();
+        }
+    }
+}

6cdbb1f6cbf61a1f2ec699ea7d6c1ffca8360137
Atlant5000 <maximmmd13@gmail.com>
2024-11-25 17:35:07 +0200

    Task 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Input();
+        }
+        /// <summary>
+        /// Метод вывода
+        /// </summary>
+        public static void Input()
+        {
+            Console.WriteLine($"x={Math.Round(Func(5, 7) + Func(12, 8) + Func(19, 2))}");
+            Console.Read();
+        }
+        /// <summary>
+        /// Метод функции
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double Func(int a, int b)
+        {
+            return (Math.Sqrt(a) + a / Math.Sqrt(b) + b);
+        }
+    }
+}
